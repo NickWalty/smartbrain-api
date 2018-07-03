ccc58ff5dabf3482d620b038bf817c2cfa76dbf5
NickWalty <nick.bell@gmail.com>
2018-07-03 16:06:54 -0300

    Added port in server

--- server.js
+++ server.js
@@ -67,6 +67,6 @@ app.put('/image', (req,res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req,res) => {image.handleAPICall(req, res)})
 
 //Does it compile???
-app.listen(3000, () => {
-	console.log('app is now running on port 3000.');
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is now running on port ${process.env.PORT}.``);
 })
